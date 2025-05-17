af3816e00dabcc936bc40ebe30dc3317d3d6aebc
HideBa <baba.papa1120.ba@gmail.com>
2025-05-17 10:26:41 +0200

    chore: lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { Allotment } from "allotment";
 import * as Cesium from "cesium";
 import {
-  CameraLookAt,
-  Entity,
-  ImageryLayer,
-  PointGraphics,
-  RectangleGraphics,
-  Scene,
-  ScreenSpaceCameraController,
-  ScreenSpaceEvent,
-  ScreenSpaceEventHandler,
-  Viewer,
+	CameraLookAt,
+	Entity,
+	ImageryLayer,
+	PointGraphics,
+	RectangleGraphics,
+	Scene,
+	ScreenSpaceCameraController,
+	ScreenSpaceEvent,
+	ScreenSpaceEventHandler,
+	Viewer,
 } from "resium";
 import "allotment/dist/style.css";
 import { useAtom } from "jotai";
@@ -23,102 +23,102 @@ import CjPreviewer from "./components/cjpreviewer";
 import { Spinner } from "./components/spinner";
 import DataFetchControls from "./feature/data-fetch-controls";
 import {
-  dataExtentAtom,
-  isLoadingAtom,
-  pointAtom,
-  rectangleAtom,
-  spatialQueryTypeAtom,
+	dataExtentAtom,
+	isLoadingAtom,
+	pointAtom,
+	rectangleAtom,
+	spatialQueryTypeAtom,
 } from "./store";
 
 function App() {
-  const {
-    viewerRef,
-    handleMouseDown,
-    handleMouseMove,
-    handleMouseUp,
-    isDrawMode,
-    handleToggleDrawMode,
-    intermediateRectangle,
-    handlePointClick,
-  } = useCesiumControls();
+	const {
+		viewerRef,
+		handleMouseDown,
+		handleMouseMove,
+		handleMouseUp,
+		isDrawMode,
+		handleToggleDrawMode,
+		intermediateRectangle,
+		handlePointClick,
+	} = useCesiumControls();
 
-  // Get atoms for display and control
-  const [rectangle] = useAtom(rectangleAtom);
-  const [point] = useAtom(pointAtom);
-  const [spatialQueryType] = useAtom(spatialQueryTypeAtom);
-  const [isLoading] = useAtom(isLoadingAtom);
-  const [dataExtent] = useAtom(dataExtentAtom);
+	// Get atoms for display and control
+	const [rectangle] = useAtom(rectangleAtom);
+	const [point] = useAtom(pointAtom);
+	const [spatialQueryType] = useAtom(spatialQueryTypeAtom);
+	const [isLoading] = useAtom(isLoadingAtom);
+	const [dataExtent] = useAtom(dataExtentAtom);
 
-  const delftLatLng = Cesium.Cartesian3.fromDegrees(
-    4.369802767481661,
-    52.00151347611216,
-    1000
-  );
-  const offset = new Cesium.Cartesian3(0, 0, 1000);
+	const delftLatLng = Cesium.Cartesian3.fromDegrees(
+		4.369802767481661,
+		52.00151347611216,
+		1000,
+	);
+	const offset = new Cesium.Cartesian3(0, 0, 1000);
 
-  // Data URL
-  const fcbUrl =
-    "https://storage.googleapis.com/flatcitybuf/3dbag_subset_all_index.fcb";
+	// Data URL
+	const fcbUrl =
+		"https://storage.googleapis.com/flatcitybuf/3dbag_subset_all_index.fcb";
 
-  // FCB data handler
-  const {
-    result,
-    handleFetchFcb,
-    handleFetchFcbWithPoint,
-    handleFetchFcbWithAttributeConditions,
-    loadNextBatch,
-    handleCjSeqDownload,
-  } = useFcbData({
-    fcbUrl,
-  });
+	// FCB data handler
+	const {
+		result,
+		handleFetchFcb,
+		handleFetchFcbWithPoint,
+		handleFetchFcbWithAttributeConditions,
+		loadNextBatch,
+		handleCjSeqDownload,
+	} = useFcbData({
+		fcbUrl,
+	});
 
-  const apiKey = import.meta.env.VITE_GEOAPIFY_API_KEY;
-  const mapUrl = `https://maps.geoapify.com/v1/tile/toner-grey/{z}/{x}/{y}.png?&apiKey=${apiKey}`;
-  const imageryProvider = new Cesium.UrlTemplateImageryProvider({
-    url: mapUrl,
-    credit: "© Geoapify, © OpenStreetMap contributors",
-  });
+	const apiKey = import.meta.env.VITE_GEOAPIFY_API_KEY;
+	const mapUrl = `https://maps.geoapify.com/v1/tile/toner-grey/{z}/{x}/{y}.png?&apiKey=${apiKey}`;
+	const imageryProvider = new Cesium.UrlTemplateImageryProvider({
+		url: mapUrl,
+		credit: "© Geoapify, © OpenStreetMap contributors",
+	});
 
-  // Is point or rectangle selection active
-  const isPointMode =
-    spatialQueryType === "pointIntersects" ||
-    spatialQueryType === "pointNearest";
+	// Is point or rectangle selection active
+	const isPointMode =
+		spatialQueryType === "pointIntersects" ||
+		spatialQueryType === "pointNearest";
 
-  return (
-    <div className="h-screen w-screen">
-      <Allotment vertical>
-        <Allotment.Pane>
-          <div className="relative h-full">
-            <Viewer
-              ref={viewerRef}
-              infoBox={false}
-              homeButton={false}
-              baseLayerPicker={false}
-              navigationHelpButton={false}
-              timeline={false}
-              animation={false}
-              fullscreenButton={false}
-              geocoder={false}
-              sceneModePicker={false}
-            >
-              <Scene />
+	return (
+		<div className="h-screen w-screen">
+			<Allotment vertical>
+				<Allotment.Pane>
+					<div className="relative h-full">
+						<Viewer
+							ref={viewerRef}
+							infoBox={false}
+							homeButton={false}
+							baseLayerPicker={false}
+							navigationHelpButton={false}
+							timeline={false}
+							animation={false}
+							fullscreenButton={false}
+							geocoder={false}
+							sceneModePicker={false}
+						>
+							<Scene />
 
-              <CameraLookAt
-                // Center of the netherlands
-                offset={offset}
-                once
-                target={delftLatLng}
-              />
+							<CameraLookAt
+								// Center of the netherlands
+								offset={offset}
+								once
+								target={delftLatLng}
+							/>
 
-              <ScreenSpaceCameraController
-                enableRotate={!isDrawMode}
-                enableTranslate={true}
-                enableTilt={!isDrawMode}
-                enableLook={!isDrawMode}
-              />
+							<ScreenSpaceCameraController
+								enableRotate={!isDrawMode}
+								enableTranslate={true}
+								enableTilt={!isDrawMode}
+								enableLook={!isDrawMode}
+							/>
 
-              <ImageryLayer imageryProvider={imageryProvider} />
-              {/* <ImageryLayer
+							<ImageryLayer imageryProvider={imageryProvider} />
+							{/* <ImageryLayer
                 imageryProvider={
                   new Cesium.OpenStreetMapImageryProvider({
                     url: "https://a.tile.openstreetmap.org/",
@@ -126,155 +126,155 @@ function App() {
                   })
                 }
               /> */}
-              <ScreenSpaceEventHandler>
-                <ScreenSpaceEvent
-                  action={handleMouseDown}
-                  type={Cesium.ScreenSpaceEventType.LEFT_DOWN}
-                />
-                <ScreenSpaceEvent
-                  action={handleMouseMove}
-                  type={Cesium.ScreenSpaceEventType.MOUSE_MOVE}
-                />
-                <ScreenSpaceEvent
-                  action={handleMouseUp}
-                  type={Cesium.ScreenSpaceEventType.LEFT_UP}
-                />
-                <ScreenSpaceEvent
-                  action={handlePointClick}
-                  type={Cesium.ScreenSpaceEventType.LEFT_CLICK}
-                />
-              </ScreenSpaceEventHandler>
+							<ScreenSpaceEventHandler>
+								<ScreenSpaceEvent
+									action={handleMouseDown}
+									type={Cesium.ScreenSpaceEventType.LEFT_DOWN}
+								/>
+								<ScreenSpaceEvent
+									action={handleMouseMove}
+									type={Cesium.ScreenSpaceEventType.MOUSE_MOVE}
+								/>
+								<ScreenSpaceEvent
+									action={handleMouseUp}
+									type={Cesium.ScreenSpaceEventType.LEFT_UP}
+								/>
+								<ScreenSpaceEvent
+									action={handlePointClick}
+									type={Cesium.ScreenSpaceEventType.LEFT_CLICK}
+								/>
+							</ScreenSpaceEventHandler>
 
-              {/* Display the data extent */}
-              {dataExtent && (
-                <Entity>
-                  <RectangleGraphics
-                    coordinates={dataExtent}
-                    material={Cesium.Color.GRAY.withAlpha(0.2)}
-                    outline={true}
-                    outlineColor={Cesium.Color.GRAY}
-                  />
-                </Entity>
-              )}
-              {/* Display the intermediateRectangle */}
-              {intermediateRectangle && !isPointMode && (
-                <Entity
-                  rectangle={{
-                    coordinates: intermediateRectangle,
-                    material: Cesium.Color.GRAY.withAlpha(0.5),
-                    outline: true,
-                    outlineColor: Cesium.Color.GRAY,
-                  }}
-                />
-              )}
+							{/* Display the data extent */}
+							{dataExtent && (
+								<Entity>
+									<RectangleGraphics
+										coordinates={dataExtent}
+										material={Cesium.Color.GRAY.withAlpha(0.2)}
+										outline={true}
+										outlineColor={Cesium.Color.GRAY}
+									/>
+								</Entity>
+							)}
+							{/* Display the intermediateRectangle */}
+							{intermediateRectangle && !isPointMode && (
+								<Entity
+									rectangle={{
+										coordinates: intermediateRectangle,
+										material: Cesium.Color.GRAY.withAlpha(0.5),
+										outline: true,
+										outlineColor: Cesium.Color.GRAY,
+									}}
+								/>
+							)}
 
-              {/* Display the completed rectangle */}
-              {rectangle && !isPointMode && (
-                <Entity
-                  rectangle={{
-                    coordinates: rectangle,
-                    material: Cesium.Color.GRAY.withAlpha(0.7),
-                    outline: true,
-                    outlineColor: Cesium.Color.GRAY,
-                  }}
-                />
-              )}
+							{/* Display the completed rectangle */}
+							{rectangle && !isPointMode && (
+								<Entity
+									rectangle={{
+										coordinates: rectangle,
+										material: Cesium.Color.GRAY.withAlpha(0.7),
+										outline: true,
+										outlineColor: Cesium.Color.GRAY,
+									}}
+								/>
+							)}
 
-              {/* Display the point for point-based spatial queries */}
-              {point && isPointMode && (
-                <Entity position={point}>
-                  <PointGraphics
-                    pixelSize={10}
-                    color={Cesium.Color.GRAY}
-                    outlineColor={Cesium.Color.WHITE}
-                    outlineWidth={2}
-                  />
-                </Entity>
-              )}
-            </Viewer>
-            {/* Info Popup */}
-            <div className="absolute top-4 right-4 z-10">
-              <InfoPopup
-                title="FlatCityBuf Prototype"
-                description="This application is a prototype for FlatCityBuf, a cloud-optimized CityJSON format for efficient city data streaming."
-                description2="The data is derived from the 3DBAG dataset, the subset of 3DBAG data. (approx 3GB, 529480 CityJSON features)"
-              />
-            </div>
+							{/* Display the point for point-based spatial queries */}
+							{point && isPointMode && (
+								<Entity position={point}>
+									<PointGraphics
+										pixelSize={10}
+										color={Cesium.Color.GRAY}
+										outlineColor={Cesium.Color.WHITE}
+										outlineWidth={2}
+									/>
+								</Entity>
+							)}
+						</Viewer>
+						{/* Info Popup */}
+						<div className="absolute top-4 right-4 z-10">
+							<InfoPopup
+								title="FlatCityBuf Prototype"
+								description="This application is a prototype for FlatCityBuf, a cloud-optimized CityJSON format for efficient city data streaming."
+								description2="The data is derived from the 3DBAG dataset, the subset of 3DBAG data. (approx 3GB, 529480 CityJSON features)"
+							/>
+						</div>
 
-            <div className="absolute top-4 left-4 z-10 flex flex-col gap-2 w-80">
-              <DataFetchControls
-                handleFetchFcb={handleFetchFcb}
-                handleToggleDrawMode={handleToggleDrawMode}
-                isDrawMode={isDrawMode}
-                handleFetchFcbWithPoint={handleFetchFcbWithPoint}
-                handleFetchFcbWithAttributeConditions={
-                  handleFetchFcbWithAttributeConditions
-                }
-                loadNextBatch={loadNextBatch}
-                handleCjSeqDownload={handleCjSeqDownload}
-                hasRectangle={!!rectangle}
-                hasPoint={!!point}
-              />
+						<div className="absolute top-4 left-4 z-10 flex flex-col gap-2 w-80">
+							<DataFetchControls
+								handleFetchFcb={handleFetchFcb}
+								handleToggleDrawMode={handleToggleDrawMode}
+								isDrawMode={isDrawMode}
+								handleFetchFcbWithPoint={handleFetchFcbWithPoint}
+								handleFetchFcbWithAttributeConditions={
+									handleFetchFcbWithAttributeConditions
+								}
+								loadNextBatch={loadNextBatch}
+								handleCjSeqDownload={handleCjSeqDownload}
+								hasRectangle={!!rectangle}
+								hasPoint={!!point}
+							/>
 
-              {isLoading && (
-                <div className="absolute inset-0 flex justify-center items-center pointer-events-none">
-                  <div className="p-4 bg-white/50 rounded-md backdrop-blur-md border border-neutral-200 shadow-md">
-                    <Spinner />
-                  </div>
-                </div>
-              )}
-            </div>
+							{isLoading && (
+								<div className="absolute inset-0 flex justify-center items-center pointer-events-none">
+									<div className="p-4 bg-white/50 rounded-md backdrop-blur-md border border-neutral-200 shadow-md">
+										<Spinner />
+									</div>
+								</div>
+							)}
+						</div>
 
-            {/* Data Attribution */}
-            <div className="absolute bottom-2 right-2 z-10 text-xs text-neutral-600 bg-white/70 px-2 py-1 rounded-sm">
-              Data: ©{" "}
-              <a
-                href="https://3dbag.nl"
-                className="underline"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                3DBAG
-              </a>{" "}
-              | Map: © Powered by{" "}
-              <a
-                href="https://www.geoapify.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Geoapify
-              </a>{" "}
-              |{" "}
-              <a
-                href="https://openmaptiles.org/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                OpenMapTiles
-              </a>{" "}
-              |{" "}
-              <a
-                href="https://www.openstreetmap.org/copyright"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                © OpenStreetMap
-              </a>{" "}
-              contributors
-            </div>
-          </div>
-        </Allotment.Pane>
-        <Allotment.Pane className="h-full">
-          {/* isLoading ? (
+						{/* Data Attribution */}
+						<div className="absolute bottom-2 right-2 z-10 text-xs text-neutral-600 bg-white/70 px-2 py-1 rounded-sm">
+							Data: ©{" "}
+							<a
+								href="https://3dbag.nl"
+								className="underline"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								3DBAG
+							</a>{" "}
+							| Map: © Powered by{" "}
+							<a
+								href="https://www.geoapify.com/"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								Geoapify
+							</a>{" "}
+							|{" "}
+							<a
+								href="https://openmaptiles.org/"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								OpenMapTiles
+							</a>{" "}
+							|{" "}
+							<a
+								href="https://www.openstreetmap.org/copyright"
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								© OpenStreetMap
+							</a>{" "}
+							contributors
+						</div>
+					</div>
+				</Allotment.Pane>
+				<Allotment.Pane className="h-full">
+					{/* isLoading ? (
           <div className="flex justify-center items-center h-full">
             <Spinner />
           </div>
           ) */}
-          <CjPreviewer result={result} />
-        </Allotment.Pane>
-      </Allotment>
-    </div>
-  );
+					<CjPreviewer result={result} />
+				</Allotment.Pane>
+			</Allotment>
+		</div>
+	);
 }
 
 export default App;
--- src/api/fcb/types.ts
+++ src/api/fcb/types.ts
@@ -2,68 +2,68 @@ import type { AsyncFeatureIter, HttpFcbReader } from "flatcitybuf";
 
 // Types for the FCB API
 export interface Column {
-  index: number;
-  name: string;
-  type: string;
-  title?: string;
-  description?: string;
-  attrIndex?: boolean;
+	index: number;
+	name: string;
+	type: string;
+	title?: string;
+	description?: string;
+	attrIndex?: boolean;
 }
 
 export interface FcbMeta {
-  columns: Column[];
-  featureCount: number;
-  dataExtent: DataExtent;
+	columns: Column[];
+	featureCount: number;
+	dataExtent: DataExtent;
 }
 
 export interface DataExtent {
-  minX: number;
-  minY: number;
-  minZ: number;
-  maxX: number;
-  maxY: number;
-  maxZ: number;
+	minX: number;
+	minY: number;
+	minZ: number;
+	maxX: number;
+	maxY: number;
+	maxZ: number;
 }
 
 export interface CjInfo {
-  cj: unknown;
-  features: unknown[];
-  meta: {
-    features_count: number;
-    fetched_features_count: number;
-  };
+	cj: unknown;
+	features: unknown[];
+	meta: {
+		features_count: number;
+		fetched_features_count: number;
+	};
 }
 
 export const validOperators = ["Gt", "Ge", "Eq", "Lt", "Le"] as const;
 // Types for attribute conditions
 export interface Condition {
-  attribute: string;
-  operator: (typeof validOperators)[number];
-  value: string | number;
+	attribute: string;
+	operator: (typeof validOperators)[number];
+	value: string | number;
 }
 
 export type SpatialQueryType = "bbox" | "pointIntersects" | "pointNearest";
 export type AttributeQueryType = "attr";
 
 export interface SpatialQuery {
-  type: SpatialQueryType;
-  bbox?: number[];
-  point?: number[];
+	type: SpatialQueryType;
+	bbox?: number[];
+	point?: number[];
 }
 
 export interface AttributeQuery {
-  type: AttributeQueryType;
-  conditions: Condition[];
+	type: AttributeQueryType;
+	conditions: Condition[];
 }
 
 // Store readers and iterators for reuse
 export type FcbQuery = SpatialQuery | AttributeQuery;
 
 export type ReaderState = {
-  reader: HttpFcbReader;
-  iterator: AsyncFeatureIter | undefined;
-  header: Map<string, unknown>;
-  totalCount: number;
-  currentPosition: number;
-  query: FcbQuery;
+	reader: HttpFcbReader;
+	iterator: AsyncFeatureIter | undefined;
+	header: Map<string, unknown>;
+	totalCount: number;
+	currentPosition: number;
+	query: FcbQuery;
 };
